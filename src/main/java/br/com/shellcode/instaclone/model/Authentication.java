d782acbd6106c32beb572830ba8d7771e424e167
Anderson Calixto <andersonbr@gmail.com>
2019-05-16 13:44:04 -0300

    authentication model fix

--- src/main/java/br/com/shellcode/instaclone/model/Authentication.java
+++ src/main/java/br/com/shellcode/instaclone/model/Authentication.java
@@ -1,26 +1,20 @@
 package br.com.shellcode.instaclone.model;
 
-import java.util.Set;
-
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Authentication {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Pessoa pessoa;
 	private String token;
-
-	@JsonIgnore
-	@OneToMany(mappedBy = "pessoa")
-	private Set<Posts> posts;
 
 	public Integer getId() {
 		return id;
